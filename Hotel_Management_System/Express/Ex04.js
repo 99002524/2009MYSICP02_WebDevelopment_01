275e1f927999b00811fac44e5e55e37bd590ded8
99002480 <71481082+99002480@users.noreply.github.com>
2020-11-11 14:49:26 +0530

    Update Ex04.js

--- Hotel_Management_System/Express/Ex04.js
+++ Hotel_Management_System/Express/Ex04.js
@@ -1,9 +1,1 @@
-const app = require("express")();
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/BookRooms.html");
-})
-
-app.listen(3333, () => {
-    console.log("Client App running at 3333");
-})
