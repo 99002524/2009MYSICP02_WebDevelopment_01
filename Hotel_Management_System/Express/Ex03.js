406d2aa5d39f81af539e97d2ef920fe27b9c3e86
99002480 <71481082+99002480@users.noreply.github.com>
2020-11-11 14:50:38 +0530

    Update Ex03.js

--- Hotel_Management_System/Express/Ex03.js
+++ Hotel_Management_System/Express/Ex03.js
@@ -1,4 +1,14 @@
 
+const app = require('express')();
+const parser = require("body-parser");
+const fs = require("fs");
+const dir = __dirname;
+
+//middleware....
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+
 //GET(Reading), POST(Adding), PUT(Updating), DELETE(Deleting) data....
 let user = []; //blank array...
 let flag = 1;
